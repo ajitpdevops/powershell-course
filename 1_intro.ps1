64cf46d9eef2331e53acdc9a5150260ef9beb6db
Ajit_Patil <ajit_patil@persistent.com>
2022-02-06 08:28:19 +0530

    init

---
+++ 1_intro.ps1
@@ -0,0 +1,30 @@
+$PSVersionTable.PSVersion
+$host 
+Get-Host
+Get-HotFix
+Get-PSReadLineKeyHandler
+
+get-aduser
+
+Get-Service -Name wuauserv
+Get-Service -Name wuauserv,wsearch
+
+Get-Help Get-Service
+help Get-Service
+Get-Help Get-Service -Full
+help Get-Service -ShowWindow
+help about*
+
+
+Get-Command *user*
+Get-Command *user* -Module ActiveDirectory
+Get-Command *user* -CommandType Application
+
+Find-Command 
+Find-Command -Tag NTFS
+Show-Command
+
+Get-Module
+Get-Module -ListAvailable
+Find-Module Az
+Find-Module Az -AllVersions
---
+++ Info.md
@@ -0,0 +1,14 @@
+# Overview of powershell
+- History 
+- CLI one ers vs Scrpting 
+- Console vs Editors 
+- cmdlets, Function. Aliases, and Workflows 
+- Windows ps vs oS 
+# Powershell vs Version 
+- 32 vs 64 
+
+# Basic coomands 
+- Get-Service -Name wuauserv
+
+- psgallery 
+- 
